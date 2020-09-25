d13c66596310fc5a32bd8fc2c74f0662b19abe42
praveenupadhyay997 <praveenupadhyay997@gmail.com>
2020-09-25 16:06:44 +0530

    My Second Commit

---
+++ Factorial/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            int factorial = 1;
+
+            for (int i = 1; i <= num; i++)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine("The Factrial of number {0} is {1}", num, factorial);
+        }
+    }
+}
