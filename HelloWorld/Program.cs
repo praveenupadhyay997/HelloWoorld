298830852262d95d3b085a550657ef97be0cb488
praveenupadhyay997 <praveenupadhyay997@gmail.com>
2020-09-25 14:49:21 +0530

    My First Commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is ");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
