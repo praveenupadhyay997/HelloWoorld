4ac0cfbd7916096cc994b0ff8b02543d5d2879c2
praveenupadhyay997 <praveenupadhyay997@gmail.com>
2020-09-27 22:21:49 +0530

    Added the Employee Wage Computation Problem Code

---
+++ EmployeeWagesProblem/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeWagesProblem
+{
+    class Program
+    {
+
+        public static void Display()
+        {
+            Console.WriteLine("=============================================");
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            Console.WriteLine("=============================================");
+        }
+
+        static void Main(string[] args)
+        {
+            Display();
+            
+        }
+    }
+}
